4a4da6314fc97d6448f558170965729ed75e0512
Vanessa <vanessa.y.cui@gmail.com>
2023-01-01 20:22:44 -0500

    finish tictactoe main functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+//a function defining each square. 
+function Square({value, onSquareClick}){
+  //value stares the value, setValue is a function that changes the value.
+  //null is a initial value
+  //when parent is calling a square, it will set a value to be shown on the button
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <button className="square" onClick={onSquareClick}>{value}</button>
+  )
 }
 
-export default App;
+function calculateWinner(squares){
+  const lines=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i=0; i<lines.length; i++){
+    const[a,b,c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+//this is a component
+export default function Board() {
+  //if you want to collect data from multiple children, declare shared state in the parent.
+  //useState() sets all square state to corresponding element in array
+  //pass corresponding array element to the square children. 
+  //the value will either be X, O or null
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+
+  function handleClick(i){
+    //if square is not null, skip 
+    if (squares[i] || calculateWinner(squares)){
+      return;
+    }
+
+    //create a shallow copy of the squares array 
+    const nextSquares = squares.slice()
+
+    if (xIsNext){
+      nextSquares[i] = "X"
+
+    } else {
+      nextSquares[i] = "O"
+    }
+
+    //tells react that the state of the component has changed. react re-renders the board the child components
+    setSquares(nextSquares)
+    setXIsNext(!xIsNext)
+
+  }
+
+
+  //returns a JSX element
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={()=>handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={()=>handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={()=>handleClick(2)}/>
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={()=>handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={()=>handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={()=>handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={()=>handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={()=>handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={()=>handleClick(8)}/>
+      </div>
+  </>
+  
+  )
+}
